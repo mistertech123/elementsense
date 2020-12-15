487da78930dafbd03368e35a8e8415de48e4b788
mistertech123 <bowmanjd+caleb@gmail.com>
2020-12-15 17:24:48 -0500

    Edited Icons in side nav bar

--- app/src/main/java/com/example/elementsense/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/elementsense/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Welcome to ElementSense BETA"
     }
     val text: LiveData<String> = _text
 }
