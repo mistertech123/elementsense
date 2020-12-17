988844981ddc47c4b26c99e4b7e1c8d87e3622ea
mistertech123 <bowmanjd+caleb@gmail.com>
2020-12-17 18:43:02 -0500

    Removed unnecessary files

--- app/src/main/java/com/example/elementsense/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/elementsense/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Welcome to ElementSense BETA"
+        value = "Hello World"
     }
     val text: LiveData<String> = _text
 }
