2286ddf8826a945b9e5d3a45c66fae424a08383a
mistertech123 <bowmanjd+caleb@gmail.com>
2020-12-16 19:48:07 -0500

    Fixed errors due to toolbar change.

--- app/src/main/java/com/example/elementsense/MainActivity.kt
+++ app/src/main/java/com/example/elementsense/MainActivity.kt
@@ -7,7 +7,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +16,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val toolbar: Toolbar = findViewById(R.id.toolbar)
-        setSupportActionBar(toolbar)
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
