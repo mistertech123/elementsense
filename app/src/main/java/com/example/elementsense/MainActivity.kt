b93ac828dd2bfc88953fbf7374dda1aa00d8541d
Jonathan Bowman <git@bowmanjd.com>
2020-12-17 20:01:52 -0500

    feat: working proof-of-concept using date/time

--- app/src/main/java/com/example/elementsense/MainActivity.kt
+++ app/src/main/java/com/example/elementsense/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.elementsense
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    fun update() {
+        val simpleDateFormat = SimpleDateFormat("yyyy.MM.dd G 'at' HH:mm:ss z")
+        val currentDateAndTime: String = simpleDateFormat.format(Date())
+        val textView: TextView = findViewById(R.id.dateAndTime)
+        textView.text = currentDateAndTime
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        update()
+    }
+    override fun onRestart() {
+        super.onRestart()
+        update()
     }
 }
